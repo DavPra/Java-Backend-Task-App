6a9ca63ce0057349834994d400684dd7fb7cb4fb
David Praschak <david.praschak@univie.ac.at>
2023-09-30 13:55:45 +0200

    Exceptions fixed

--- src/main/java/at/codersbay/java/taskapp/rest/services/BusinessServices.java
+++ src/main/java/at/codersbay/java/taskapp/rest/services/BusinessServices.java
@@ -4,15 +4,11 @@ import at.codersbay.java.taskapp.rest.DAO.*;
 import at.codersbay.java.taskapp.rest.entities.*;
 import at.codersbay.java.taskapp.rest.exceptions.*;
 
-import org.hibernate.PropertyValueException;
-
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 import javax.annotation.PostConstruct;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Service
 public class BusinessServices {
@@ -34,45 +30,127 @@ public class BusinessServices {
         System.out.println("BusinessServices created");
     }
 
-    public User createUser (User user) throws UserException {
+    public User createUser (User user) throws UserAlreadyExistsException {
         if (user == null) {
-            throw new UserException("User is null");
+            throw new UserAlreadyExistsException("User is null");
         }
         if (user.getId() != null) {
-            throw new UserException("User already exists");
+            throw new UserAlreadyExistsException("User already exists");
         }
         try {
             userDAO.save(user);
         } catch (Exception e) {
-            throw new UserException("User could not be created");
+            throw new UserAlreadyExistsException("User could not be created");
         }
         return user;
     }
 
-    public User getUser(Long id) throws UserException {
+    public List<User> getAllUsers() {
+
+        return userDAO.findAll();
+    }
+
+    public User getUser(Long id) throws UserNotFoundException {
         if (id == null) {
-            throw new UserException("Id is null");
+            throw new UserNotFoundException("Id is null");
         }
         Optional<User> user = userDAO.findById(id);
         if (!user.isPresent()) {
-            throw new UserException("User not found");
+            throw new UserNotFoundException("User not found");
         }
         return user.get();
     }
 
-    public User deleteUser(Long id) throws UserException {
+    public User deleteUser(Long id) throws UserNotFoundException {
         if (id == null) {
-            throw new UserException("Id is null");
+            throw new UserNotFoundException("Id is null");
         }
         Optional<User> user = userDAO.findById(id);
         if (!user.isPresent()) {
-            throw new UserException("User not found");
+            throw new UserNotFoundException("User not found");
         }
         try {
             userDAO.delete(user.get());
         } catch (Exception e) {
-            throw new UserException("User could not be deleted");
+            throw new UserNotFoundException("User could not be deleted");
         }
         return user.get();
     }
+
+    public Task createTask (Task task) throws TaskNotFoundException {
+        if (task == null) {
+            throw new TaskNotFoundException("Task is null");
+        }
+        if (task.getId() != null) {
+            throw new TaskNotFoundException("Task already exists");
+        }
+        try {
+            taskDAO.save(task);
+        } catch (Exception e) {
+            throw new TaskNotFoundException("Task could not be created");
+        }
+        return task;
+    }
+
+    public Task getTaskByTaskID(Long id) throws TaskNotFoundException {
+        if (id == null) {
+            throw new TaskNotFoundException("Id is null");
+        }
+        Optional<Task> task = taskDAO.findById(id);
+        if (!task.isPresent()) {
+            throw new TaskNotFoundException("Task not found");
+        }
+        return task.get();
+    }
+
+    public Task deleteTaskByTaskID(Long id) throws TaskNotFoundException {
+        if (id == null) {
+            throw new TaskNotFoundException("Id is null");
+        }
+        Optional<Task> task = taskDAO.findById(id);
+        if (!task.isPresent()) {
+            throw new TaskNotFoundException("Task not found");
+        }
+        try {
+            taskDAO.delete(task.get());
+        } catch (Exception e) {
+            throw new TaskNotFoundException("Task could not be deleted");
+        }
+        return task.get();
+    }
+
+    public Task updateTaskByTaskID(Long id, Task task) throws TaskNotFoundException {
+        if (id == null) {
+            throw new TaskNotFoundException("Id is null");
+        }
+        Optional<Task> taskToUpdate = taskDAO.findById(id);
+        if (!taskToUpdate.isPresent()) {
+            throw new TaskNotFoundException("Task not found");
+        }
+        if (task == null) {
+            throw new TaskNotFoundException("Task is null");
+        }
+        if (task.getId() != null) {
+            throw new TaskNotFoundException("Task already exists");
+        }
+        try {
+            taskDAO.save(task);
+        } catch (Exception e) {
+            throw new TaskNotFoundException("Task could not be updated");
+        }
+        return task;
+    }
+
+   /* public List<Task> getTasksByUserID (Long id) throws TaskNotFoundException {
+        if (id == null) {
+            throw new TaskNotFoundException("Id is null");
+        }
+        List<Task> tasks = taskDAO.findByUserId(id);
+        if (tasks.isEmpty()) {
+            throw new TaskNotFoundException("Tasks not found");
+        }
+        return tasks;
+    }
+    */
+
 }
