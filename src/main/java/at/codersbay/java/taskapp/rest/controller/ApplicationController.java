38ff50742c89f238960d07860717d817d60b5917
David Praschak <david.praschak@univie.ac.at>
2023-09-30 13:55:08 +0200

    ApplicationController generated, Methods get all Users and Create User implemented

---
+++ src/main/java/at/codersbay/java/taskapp/rest/controller/ApplicationController.java
@@ -0,0 +1,55 @@
+package at.codersbay.java.taskapp.rest.controller;
+
+import at.codersbay.java.taskapp.rest.entities.Task;
+import at.codersbay.java.taskapp.rest.entities.User;
+import at.codersbay.java.taskapp.rest.entities.Profile;
+import at.codersbay.java.taskapp.rest.exceptions.*;
+import at.codersbay.java.taskapp.rest.services.BusinessServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class ApplicationController {
+
+    @Autowired
+    private BusinessServices businessServices;
+
+    @GetMapping("/hi")
+    String welcome() {
+        return "Hello";
+    }
+
+    @GetMapping("/error")
+    String error() {
+        return "it gets hang to the error. Try enjoying surfing.";
+    }
+
+    @CrossOrigin
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return businessServices.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    User createUser(@RequestBody User user) throws UserAlreadyExistsException {
+
+        HttpStatus status = null;
+        String message = "";
+        try {
+            user = businessServices.createUser(user);
+            status = HttpStatus.OK;
+            message = "User created";
+        } catch (UserAlreadyExistsException UAEE) {
+            status = HttpStatus.CONFLICT;
+            message = UAEE.getMessage();
+        }
+        return user;
+    }
+
+
+}
