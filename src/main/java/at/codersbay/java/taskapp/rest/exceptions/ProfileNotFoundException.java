01c3cf2df2265a69d01a1ca783e838ed66429ee3
David Praschak <david.praschak@univie.ac.at>
2023-10-02 18:41:33 +0200

    Profile not found exception added.

---
+++ src/main/java/at/codersbay/java/taskapp/rest/exceptions/ProfileNotFoundException.java
@@ -0,0 +1,7 @@
+package at.codersbay.java.taskapp.rest.exceptions;
+
+public class ProfileNotFoundException extends Exception{
+    public ProfileNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/at/codersbay/java/taskapp/rest/restapi/testImpl.java
+++ src/main/java/at/codersbay/java/taskapp/rest/restapi/testImpl.java
@@ -1,4 +1,5 @@
 package at.codersbay.java.taskapp.rest.restapi;
 
+
 public class testImpl {
 }
