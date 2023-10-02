e78209c35b62115cb1e5aee602dda11f94a3d069
David Praschak <david.praschak@univie.ac.at>
2023-10-02 18:42:22 +0200

    query for find user by ID added

--- src/main/java/at/codersbay/java/taskapp/rest/DAO/ProfileDAO.java
+++ src/main/java/at/codersbay/java/taskapp/rest/DAO/ProfileDAO.java
@@ -2,8 +2,12 @@ package at.codersbay.java.taskapp.rest.DAO;
 
 import at.codersbay.java.taskapp.rest.entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfileDAO extends JpaRepository<Profile, Long>{
+
+    @Query("SELECT p FROM Profile p WHERE p.id = :id")
+    Profile findByUserId(Long id);
 }
