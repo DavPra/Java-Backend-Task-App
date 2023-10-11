dbd585803492c650d32bf9ac8e45166e2123a0b4
David Praschak <david.praschak@univie.ac.at>
2023-10-11 19:25:06 +0200

    SetUpService fixed, needs adding of profiles

--- src/main/java/at/codersbay/java/taskapp/rest/services/SetUpService.java
+++ src/main/java/at/codersbay/java/taskapp/rest/services/SetUpService.java
@@ -4,47 +4,41 @@ import at.codersbay.java.taskapp.rest.DAO.*;
 import at.codersbay.java.taskapp.rest.entities.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
+@Service
 public class SetUpService {
 
-    @Autowired
-    UserDAO userDAO;
+    private final UserDAO userDAO;
+    private final TaskDAO taskDAO;
 
     @Autowired
-    TaskDAO taskDAO;
-
-    @Autowired
-    ProfileDAO profileDAO;
-
-    public SetUpService() {
+    public SetUpService(UserDAO userDAO, TaskDAO taskDAO) {
+        this.userDAO = userDAO;
+        this.taskDAO = taskDAO;
     }
 
     @PostConstruct
-    public void setUp() {
-
+    private void setUp() {
         User admin = new User();
         admin.setFirstName("David");
         admin.setLastName("Praschak");
         admin.setEmail("david.praschak@test.org");
-
         admin = this.userDAO.save(admin);
 
         User user = new User();
         user.setFirstName("Max");
         user.setLastName("Mustermann");
         user.setEmail("max@mustermann.org");
-
         user = this.userDAO.save(user);
-
 
         Task task1 = new Task();
         task1.setTitel("Task 1");
         task1.setBeschreibung("Beschreibung 1");
         task1.setDueDate("01.01.2020");
         task1.setDone(false);
-
         task1 = this.taskDAO.save(task1);
 
         Task task2 = new Task();
@@ -52,7 +46,6 @@ public class SetUpService {
         task2.setBeschreibung("Beschreibung 2");
         task2.setDueDate("02.02.2020");
         task2.setDone(false);
-
+        task2 = this.taskDAO.save(task2);
     }
-
 }
