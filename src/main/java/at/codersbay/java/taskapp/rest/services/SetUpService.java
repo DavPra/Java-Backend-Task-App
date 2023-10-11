c0df80047fdbbbdedd1071696b627e642910c0b5
David Praschak <david.praschak@univie.ac.at>
2023-10-11 19:35:29 +0200

    added profiles to set up but connection with users not working yet

--- src/main/java/at/codersbay/java/taskapp/rest/services/SetUpService.java
+++ src/main/java/at/codersbay/java/taskapp/rest/services/SetUpService.java
@@ -13,11 +13,13 @@ public class SetUpService {
 
     private final UserDAO userDAO;
     private final TaskDAO taskDAO;
+    private final ProfileDAO profileDAO;
 
     @Autowired
-    public SetUpService(UserDAO userDAO, TaskDAO taskDAO) {
+    public SetUpService(UserDAO userDAO, TaskDAO taskDAO, ProfileDAO profileDAO) {
         this.userDAO = userDAO;
         this.taskDAO = taskDAO;
+        this.profileDAO = profileDAO;
     }
 
     @PostConstruct
@@ -47,5 +49,20 @@ public class SetUpService {
         task2.setDueDate("02.02.2020");
         task2.setDone(false);
         task2 = this.taskDAO.save(task2);
+
+        Profile profile1 = new Profile();
+        profile1.setBio("Bio 1");
+        profile1.setImage("Image 1");
+        admin.setProfile(profile1);
+        profile1.setUser(admin);
+        profile1 = this.profileDAO.save(profile1);
+
+        Profile profile2 = new Profile();
+        profile2.setBio("Bio 2");
+        profile2.setImage("Image 2");
+        user.setProfile(profile2);
+        profile2.setUser(user);
+        profile2 = this.profileDAO.save(profile2);
+
     }
 }
